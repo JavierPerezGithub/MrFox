4a92d227d4d77301f8aa75a1918939411cbde784
javierperez <javierperezjaurena@gmail.com>
2018-03-02 12:06:24 +0100

    terminado

--- app/src/main/java/com/example/javier/mrfox/MyMain.java
+++ app/src/main/java/com/example/javier/mrfox/MyMain.java
@@ -1,13 +1,50 @@
 package com.example.javier.mrfox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MyMain extends AppCompatActivity {
-
+    private SwipeRefreshLayout swipeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_main);
+        swipeLayout = findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener()
+    {
+        @Override
+        public void onRefresh() {
+
+            final LinearLayout lLayout = (LinearLayout) findViewById(R.id.activity_my_mainID);
+            Snackbar snackbar = Snackbar
+                    .make(lLayout, "lost your FOX?", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(lLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+
+            snackbar.show();
+            swipeLayout.setRefreshing(false);
+        }
+    };
 }
